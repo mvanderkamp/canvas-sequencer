28a6fa6abe48e310f854fdaedf6edd3fb4038006
mjv761 <michael.vanderkamp@usask.ca>
2019-04-27 13:33:02 -0600

    Update bundle test

--- tests/bundle.test.js
+++ tests/bundle.test.js
@@ -2,7 +2,7 @@
  * Quick test file for the bundle.
  */
 
-const { CanvasSequence, CanvasBlueprint } = require('../bundle.js');
+const { CanvasSequence, CanvasBlueprint } = require('../dist/index.js');
 
 test('Classes are available', () => {
   expect( CanvasSequence ).toBeInstanceOf(Function);
